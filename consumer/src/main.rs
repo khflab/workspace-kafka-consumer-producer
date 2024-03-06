256a178805f3ceaabc705588826c5b38998144a7
Kayo Hamid <kayohamid@outlook.com>
2024-03-06 10:14:17 -0400

    adjust: checando args input

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -2,24 +2,29 @@ use std::env;
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+  let args: Vec<String> = env::args().skip(1).collect();
 
-    let mut consumer =
-        Consumer::from_hosts(vec!("localhost:29092".to_owned()))
-            .with_topic(args[1].to_owned())
-            .with_fallback_offset(FetchOffset::Latest)
-            .with_group("salamandra-rust-dev-1".to_owned())
-            .with_offset_storage(Some(GroupOffsetStorage::Kafka))
-            .create()
-            .unwrap();
+  if args.len() == 0 { 
+    eprintln!("- Informe um tópico");
+    std::process::exit(1);
+  }
 
-    loop {
-        for ms in consumer.poll().unwrap().iter() {
-            for m in ms.messages() {
-              println!("{:?}", m);
-            }
-            consumer.consume_messageset(ms);
-          }
-          consumer.commit_consumed().unwrap();
+  let mut consumer =
+    Consumer::from_hosts(vec!("localhost:29092".to_owned()))
+      .with_topic(args[0].to_owned())
+      .with_fallback_offset(FetchOffset::Latest)
+      .with_group("salamandra-rust-dev-1".to_owned())
+      .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+      .create()
+      .unwrap();
+
+  loop {
+    for ms in consumer.poll().unwrap().iter() {
+      for m in ms.messages() {
+        println!("{:?}", m);
       }
+      consumer.consume_messageset(ms);
+    }
+    consumer.commit_consumed().unwrap();
+  }
 }
--- producer/src/main.rs
+++ producer/src/main.rs
@@ -2,7 +2,12 @@ use std::env;
 use kafka::producer::{Producer, Record};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() == 0 { 
+      eprintln!("- Informe um tópico");
+      std::process::exit(1);
+    }
 
     let hosts = vec!["localhost:29092".to_owned()];
 
@@ -13,7 +18,7 @@ fn main() {
    
     for i in 0..10 {
       let buf = format!("{i}");
-      producer.send(&Record::from_value(&args[1], buf.as_bytes())).unwrap();
+      producer.send(&Record::from_value(&args[0], buf.as_bytes())).unwrap();
       println!("Sent: {i}");
     }
 }
